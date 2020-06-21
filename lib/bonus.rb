190c6a3932b04fa7abcf4942df9face47ff00975
Carlos Rosario <carlosrosario2k@gmail.com>
2020-06-21 04:12:14 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -22,8 +22,8 @@ def bonus
 
   #code your solution here:
 
-  expect(bonus[:montague][:hero][:status]).to eq("dead")
-  expect(bonus[:capulet][:heroine][:status]).to eq("dead")
+  expect(bonus[:montague][:hero][:status]) "dead"
+  expect(bonus[:capulet][:heroine][:status]) "dead"
 
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
